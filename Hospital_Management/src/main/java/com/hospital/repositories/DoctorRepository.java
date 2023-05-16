66b23f7c8a494c8dc90eef32ededb02eace0ab0f
Nakul Srivastava <nakulsri004@gmail.com>
2023-05-16 17:21:07 +0530

    clean

--- Hospital_Management/src/main/java/com/hospital/repositories/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hospital.entities.Doctor;
-
-public interface DoctorRepository extends JpaRepository<Doctor, String>{
-
-}
